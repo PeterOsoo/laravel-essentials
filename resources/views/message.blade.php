45f8c9b2295d8908af5915a76180db5ebfb06803
peterosoo <posoo617@gmail.com>
2021-02-14 12:59:15 +0300

    style messages

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,13 +1,47 @@
 @extends('master')
+<style>
+    h3 {
+        color: rgb(43, 25, 22);
+        font-size: 1.4rem;
+        text-transform: uppercase;
+    }
+
+    .message {
+        text-align: center;
+        background: inherit;
+        margin: 0 30%;
+    }
+
+    .content {
+        padding: .4rem;
+        border: 1px solid grey;
+    }
+
+    .date {
+        text-align: end;
+        padding-right: 2rem;
+    }
+</style>
 
 
-@section('title', $message->title)
+@section('title', 'Message')
 
 
 @section('content')
 
-<h3>{{$message->title}} </h3>
-<p>{{$message->content}} </p>
+<div class="message">
+
+    <h3>{{$message->title}} </h3>
+    <p class="content">{{$message->content}} </p>
+
+    <p class="date">
+        {{-- Created at --}}
+        Posted on :
+        {{-- {{$message->created_at-> format('d-m-Y H:i')}} --}}
+        {{$message->created_at-> format('d/m/Y')}}
+    </p>
+</div>
+
 
 
 @endsection
